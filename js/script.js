2cf2ec3a0661b60cf588f2bce9edb92713352706
arkhar88 <arkhar88@gmail.com>
2017-05-14 14:29:05 +0200

    messy carousel

--- js/script.js
+++ js/script.js
@@ -1,18 +1,47 @@
-var span = $('span');
-span.css('color', 'blue');
-span.each(function(ind, elem) {
-    if (ind % 2 === 0) {
-        $(elem).css('color', 'grey');
+$(function() {
+    var carouselList = $('#js-test-carousel ul');
+    var carousel = $('#js-test-carousel');
+
+    function setCarouselMoveBtnPosition() {
+        var carouselPos = carousel.position();
+        $('.js-carousel-move-btn').css({top: carouselPos.top + carousel.height()/2 - $('.js-carousel-move-btn').height()/2});
+        $('#js-arrow-left').css({right: carouselPos.left + 80});
+        $('#js-arrow-right').css({left: carouselPos.left + 80});
+
+        $('.js-carousel-counter-container').css({top: carouselPos.top + carousel.height() - 50});
     }
-});
 
-var p = $('p');
-p.each(function(ind, elem) {
-    var btn = '<button class="btn" data-tmp="' + ind + '"> klik klik </button>';
-    $(elem).append(btn);
-});
+    // function setCarouselSlideCounter() {
+    //   carouselList.children().each(function () {
+    //     $('.js-carousel-counter-container').append('<span id="js-carousel-slide-counter" class="glyphicon glyphicon-record"></span>');
+    //   });
+    // }
 
+    function moveFirstSlide() {
+        var firstItem = carouselList.find('li:first');
+        var lastItem = carouselList.find('li:last');
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft: 0});
+    }
 
-$('button').click(function() {
-    alert($(this).attr('data-tmp'));
+    function moveLastSlide() {
+        var firstItem = carouselList.find('li:first');
+        var lastItem = carouselList.find('li:last');
+        firstItem.before(lastItem);
+        carouselList.css({marginLeft: 0});
+    }
+
+    function changeSlideRight() {
+        carouselList.animate({marginLeft: -700}, 500, moveFirstSlide);
+    }
+
+    function changeSlideLeft() {
+        moveLastSlide();
+        carouselList.css({marginLeft: -700});
+        carouselList.animate({marginLeft: 0}, 500);
+    }
+
+    setCarouselMoveBtnPosition();
+    $('#js-arrow-left.js-carousel-move-btn').click(changeSlideLeft);
+    $('#js-arrow-right.js-carousel-move-btn').click(changeSlideRight);
 });
